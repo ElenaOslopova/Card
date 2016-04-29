3bced66c19d3f3d4c4e1a499d18c3f374fa29d63
ElenaOslopova <elena_oslopova@live.ru>
2016-04-29 11:10:49 +0400

    Added files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Cards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cards = new Collection<Card>
+            {
+                new Card("Мичуринск", "Тамбов"),
+                new Card("Тверь", "Рязань"),
+                new Card("Москва", "Тверь"),                
+                new Card("Рязань", "Мичуринск")
+            };
+
+            var sc = CardsProcessor.Process(cards);
+        }
+    }
+}
